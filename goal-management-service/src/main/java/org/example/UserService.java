009d2b09635fab48fe6375481a8a6f838fff9ef6
GurgenProgrammer <gurgen.bayburdyan.10@gmail.com>
2024-11-02 10:31:06 +0400

    Update UserService.java

--- goal-management-service/src/main/java/org/example/UserService.java
+++ goal-management-service/src/main/java/org/example/UserService.java
@@ -13,16 +13,8 @@ public class UserService {
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
-
-    public User getUserById(Long id) {
-        return userRepository.findById(id).orElse(null);
-    }
-
+    
     public User saveUser(User user) {
         return userRepository.save(user);
-    }
-
-    public void deleteUser(Long id) {
-        userRepository.deleteById(id);
     }
 }
